c43c65219ab4c24cb64e312cc5d4872245139c16
ynnejthedeino <ynnejthedeino@users.noreply.github.com>
2017-06-22 16:20:51 -0700

    etempted to write words

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,15 @@ public class Player : MonoBehaviour {
 
 int Xv=0;
 int Yv=0;
+public int Pl;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetKeyDown(KeyCode.UpArrow))
+        if (Pl == 1){
+            if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             Yv = Yv + 1;
             SetPosition();
@@ -28,9 +30,28 @@ int Yv=0;
         {
             Xv = Xv + 1;
             SetPosition();
+        }} else if (Pl == 2)
+        {
+            if(Input.GetKeyDown(KeyCode.W))
+        {
+            Yv = Yv + 1;
+            SetPosition();
+        } else if(Input.GetKeyDown(KeyCode.S))
+        {
+            Yv = Yv - 1;
+            SetPosition();
+        } else if(Input.GetKeyDown(KeyCode.A))
+        {
+            Xv = Xv - 1;
+            SetPosition();
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            Xv = Xv + 1;
+            SetPosition();
         }
 
-    }
+
+    }}
 
     private void SetPosition()
     {
